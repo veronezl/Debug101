6bfe58acf5f52afabec06de9564e6e99e4a469f1
veronezl <lucasveronez.dev@gmail.com>
2025-02-12 16:47:31 -0300

    Capturar um tipo de exceção específico

--- Program.cs
+++ Program.cs
@@ -357,6 +357,7 @@ Console.WriteLine("Exit program");
 
 // Capturar exceções geradas nos métodos chamados --------------------
 
+/*
 try
 {
     Process1();
@@ -371,6 +372,82 @@ Console.WriteLine("Exit program");
 static void Process1()
 {
     WriteMessage();
+}
+
+static void WriteMessage()
+{
+    double float1 = 3000.0;
+    double float2 = 0.0;
+    int number1 = 3000;
+    int number2 = 0;
+
+    Console.WriteLine(float1 / float2);
+    Console.WriteLine(number1 / number2);
+}
+*/
+
+// Analisar a solução do desafio try-catch
+
+/*
+try
+{
+    Process1();
+}
+catch
+{
+    Console.WriteLine("An exception has occurred");
+}
+
+Console.WriteLine("Exit program");
+
+static void Process1()
+{
+    try
+    {
+        WriteMessage();
+    }
+    catch
+    {
+        Console.WriteLine("Exception caught in Process1");
+    }
+
+}
+
+static void WriteMessage()
+{
+    double float1 = 3000.0;
+    double float2 = 0.0;
+    int number1 = 3000;
+    int number2 = 0;
+
+    Console.WriteLine(float1 / float2);
+    Console.WriteLine(number1 / number2);
+}
+*/
+
+// Acessar as propriedades de um objeto de exceção -----------------------
+
+try
+{
+    Process1();
+}
+catch
+{
+    Console.WriteLine("An exception has occurred");
+}
+
+Console.WriteLine("Exit program");
+
+static void Process1()
+{
+    try
+    {
+        WriteMessage();
+    }
+    catch (DivideByZeroException ex)
+    {
+        Console.WriteLine($"Exception caught in Process1: {ex.Message}");
+    }
 }
 
 static void WriteMessage()
