22af9386a43e00e2b3c121165bac1a093d043f50
veronezl <lucasveronez.dev@gmail.com>
2025-02-12 10:15:05 -0300

    Exercicio de Implementar o tratamento de exceção try-catch e Capturar exceções geradas nos métodos chamados

--- Program.cs
+++ Program.cs
@@ -221,6 +221,8 @@ do
 This code instantiates a value and then calls the ChangeValue method
 to update the value. The code then prints the updated value to the console.
 */
+
+/*
 int x = 5;
 x = ChangeValue(x);
 
@@ -232,4 +234,152 @@ int ChangeValue(int value)
 {
     value = 10;
     return value;
+}
+*/
+
+// Exemplos de código para exceções geradas pelo compilador -------------------
+
+// ArrayTypeMismatchException:
+
+// Uma exceção do tipo ArrayTypeMismatchException é gerada quando há uma 
+// tentativa de armazenar um elemento do tipo errado em uma matriz. Ex:
+
+/*
+string[] names = { "Dog", "Cat", "Fish" };
+Object[] objs = (Object[])names;
+
+Object obj = (Object)13;
+objs[2] = obj; // ArrayTypeMismatchException occurs
+*/
+
+// DivideByZeroException
+
+// Uma exceção do tipo DivideByZeroException ocorre quando há uma tentativa de
+// dividir um número inteiro ou decimal por zero. Ex:
+
+/*
+int number1 = 3000;
+int number2 = 0;
+Console.WriteLine(number1 / number2); // DivideByZeroException occurs
+*/
+
+// FormatException
+
+// Uma exceção do tipo FormatException ocorre quando o formato de um argumento
+// é inválido ou quando uma cadeia de caracteres de formato composto não está
+// bem formada. Ex: 
+
+/*
+int valueEntered;
+string userValue = "two";
+valueEntered = int.Parse(userValue); // FormatException occurs
+*/
+
+// IndexOutOfRangeException
+
+// A exceção do tipo IndexOutOfRangeException é gerada quando há uma tentativa
+// de acessar um elemento de uma matriz ou de uma coleção com um índice que
+// está fora dos limites. Ex:
+
+/*
+int[] values1 = { 3, 6, 9, 12, 15, 18, 21 };
+int[] values2 = new int[6];
+
+values2[values1.Length - 1] = values1[values1.Length - 1]; // IndexOutOfRangeException occurs
+*/
+
+// InvalidCastException
+
+// Uma exceção do tipo InvalidCastException é gerada quando há uma tentativa de
+// fazer uma conversão inválida ou explícita. Ex:
+
+/*
+object obj = "This is a string";
+int num = (int)obj;
+*/
+
+// NullReferenceException
+
+// Uma exceção do tipo NullReferenceException é gerada quando há uma tentativa de acessar um membro em um tipo cujo valor é nulo.
+// Dois exemplos são mostrados.
+
+/*
+int[] values = null;
+for (int i = 0; i <= 9; i++)
+    values[i] = i * 2;
+*/
+
+// -------------------------
+
+/*
+string? lowCaseString = null;
+Console.WriteLine(lowCaseString.ToUpper());
+*/
+
+// OverflowException
+
+// Uma exceção do tipo OverflowException ocorre quando uma operação aritmética
+// tenta atribuir um resultado que está fora do intervalo do tipo de dados de 
+// destino. Ex: 
+
+/*
+decimal x = 400;
+byte i;
+
+i = (byte)x; // OverflowException occurs
+Console.WriteLine(i);
+*/
+
+// Exercício – Implementar o tratamento de exceção try-catch
+
+// Implementar um try-catch simples
+
+/*
+using System.Security.Cryptography;
+
+double float1 = 3000.0;
+double float2 = 0.0;
+int number1 = 3000;
+int number2 = 0;
+
+try
+{
+Console.WriteLine(float1 / float2);
+Console.WriteLine(number1 / number2);
+}
+catch
+{
+    System.Console.WriteLine("An exception has been caught");
+}
+
+Console.WriteLine("Exit program");
+*/
+
+// Capturar exceções geradas nos métodos chamados --------------------
+
+try
+{
+    Process1();
+}
+catch
+{
+    Console.WriteLine("An exception has occurred");
+}
+
+Console.WriteLine("Exit program");
+
+static void Process1()
+{
+    WriteMessage();
+}
+
+static void WriteMessage()
+{
+    double float1 = 3000.0;
+    double float2 = 0.0;
+    int number1 = 3000;
+    int number2 = 0;
+
+    Console.WriteLine(float1 / float2);
+    Console.WriteLine(number1 / number2);
 }
